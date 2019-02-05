ba84d4d24f9e411fb5f232782ffebb16ec1ae865
alumno <alumno@PC-XX>
2019-02-05 14:15:53 +0100

    segundo commit

--- GitHub/src/Prueba/prueba2.java
+++ GitHub/src/Prueba/prueba2.java
@@ -1,9 +1,10 @@
 package Prueba;
 
-public class Prueba {
+public class prueba2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		// TODO Auto-generated method stub
+
 	}
 
 }
